b501bea48c4a609a45f321465fb23c98615b8407
igorvasilcev <igrok458@gmail.com>
2024-04-20 12:56:58 +0300

    init commit

---
+++ src/test/java/ru/shashy/orderrestapi/OrderRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shashy.orderrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
